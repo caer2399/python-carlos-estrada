d620204dc4e58da604f0a749818e92758c5ce96a
caer2399 <caer2399@gmail.com>
2021-04-21 04:17:07 +0000

    add Pythonlab1

---
+++ placeholdes.py
@@ -0,0 +1,18 @@
+# placeaholders.py
+
+name = "mary"
+age = "19"
+city = "orizaba"
+state = "Veracruz"
+country = "Mexico"
+
+message = "My name is %s, im %s years old \n"
+print (message% (name, age));
+
+
+message = "\t My name is %s \n\t I am %s years old \n\t I am from %s, %s"
+print (message%(name, age, city, state, country))
+
+
+
+
