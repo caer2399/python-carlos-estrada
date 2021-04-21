4d3289b7c794e662a29372bb34822f689dec2ac8
caer2399 <caer2399@gmail.com>
2021-04-21 00:41:32 +0000

    add Pythonlab1

---
+++ variables.py
@@ -0,0 +1,20 @@
+#variables.py
+#autor: carlos estrada
+# marzo 26, 2021
+
+year = 2021;
+sentence = "My name is cestrada"
+sarah, bob, mike = 16, 20, 25;
+name, age = "maria", 24;
+mary = fer = 20;
+PI = 3.1416;
+e = 2.71;
+
+# display values
+print "year = ", year;
+print "sentence = ", sentence;
+print "\nsarah = ", sarah, "\nbob =", bob, "\nmike =", mike;
+print "\n\t age = ", age;
+print mary, fer
+print PI
+print e
