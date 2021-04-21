4d5eca59b8cc34566c0c8a9c625633e02ee4b17f
caer2399 <caer2399@gmail.com>
2021-04-21 03:26:01 +0000

    add Pythonlab1

---
+++ operators.py
@@ -0,0 +1,43 @@
+age1 = 9.0 
+age2 = 20.0
+
+print "\n age1 = ", age1
+print " age2 = ", age2
+
+sumage = age1 + age2
+print "\n suma (+) : ", sumage
+
+print " restar (-) : ", age1 - age2
+print " mul (*) : ", age1 * age2
+print " div (/) : ", age1 / age2
+print " res (%) : ", age2 % age1
+
+message = "Today was a beautiful day"
+firstname = "carlos"
+lastname = "estrada"
+
+print "\n" + message
+print firstname
+print lastname
+
+print "\n sum strings  (+) : ", message +  ", " + firstname+" " + lastname
+
+# adenina (A), timina (T), guanina (d) y citosina (C).
+adn = "AACTGTGAACTACGTCTTAACGACTGGTACGTAC"
+print "\nadn =", adn
+
+print "adn pos 0: ", adn [0]
+print "adn pos 4: ", adn [4]
+print "adn pos 0 to 7   : ", adn [0:7]
+print "adn pos 10 to 14 : ", adn [10:14]
+print "adn first 10     : ", adn [:10]
+print " and length - 2", adn [:-2]
+
+
+
+
+
+
+
+
+
