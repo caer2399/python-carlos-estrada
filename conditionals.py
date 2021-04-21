fd7b929f0188e427ccce8103cc5091e50df0132c
caer2399 <caer2399@gmail.com>
2021-04-21 02:44:03 +0000

    add Pythonlab1

---
+++ conditionals.py
@@ -0,0 +1,16 @@
+#if
+
+nucleotide = input("Input nucleotide A, C, T, G: ")
+print (nucleotide)
+
+if (nucleotide == "A"):
+    print ("ADENINA")
+elif (nucleotide == "C"):
+    print ("CITOSINA")
+elif (nucleotide == "T"):
+    print ("TIMINA")
+elif (nucleotide == "G"):
+    print ("GUANINA")
+else: 
+    print ("ERROR in input")
+
